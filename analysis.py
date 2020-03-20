6be04141fa9641f7da23555a442fd812526d4111
R W <r.wojtczak23@gmail.com>
2020-03-20 15:55:35 +0000

    just starting

--- analysis.py
+++ analysis.py
@@ -1,1 +1,9 @@
-import pands as pd
+#Importing Package 
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#To read the CSV files, we use the read_csv method, as follows and pass the filename  to a variable (iris).
+iris = pd.read_csv("iris.csv")
+print(iris.head())

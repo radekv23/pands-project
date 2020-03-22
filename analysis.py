2cfa69e7e31cd11dc68d9ecd740f4daa2f717f1b
R W <r.wojtczak23@gmail.com>
2020-03-22 14:17:36 +0000

    fix seaborn height

--- analysis.py
+++ analysis.py
@@ -25,15 +25,22 @@ print(iris["species"].value_counts())
 print(iris.describe())
 
 #1D Scatter Plots
-iris_setso = iris.loc[iris["species"] == "setosa"];
+iris_setsoa = iris.loc[iris["species"] == "setosa"];
 iris_virginica = iris.loc[iris["species"] == "virginica"];
 iris_versicolor = iris.loc[iris["species"] == "versicolor"];
-plt.plot(iris_setso["petal_length"],np.zeros_like(iris_setso["petal_length"]), 'o')
-plt.plot(iris_versicolor["petal_length"],np.zeros_like(iris_versicolor["petal_length"]), 'o')
-plt.plot(iris_virginica["petal_length"],np.zeros_like(iris_virginica["petal_length"]), 'o')
+plt.plot(iris_setsoa["petal_length"],np.zeros_like(iris_setsoa["petal_length"]), 'o', label='setosa')
+plt.plot(iris_versicolor["petal_length"],np.zeros_like(iris_versicolor["petal_length"]), 'o', label='versicolor')
+plt.plot(iris_virginica["petal_length"],np.zeros_like(iris_virginica["petal_length"]), 'o', label='virginica')
 plt.grid()
 plt.show()  
 
 # 2D Scatter Plots
 iris.plot(kind="scatter",x="sepal_length",y="sepal_width")
 plt.show() 
+
+#2D scatter plot in seaborn
+sns.set_style("whitegrid");
+sns.FacetGrid(iris,hue="species",height=4) \
+.map(plt.scatter,"sepal_length","sepal_width") \
+.add_legend()
+plt.show()  

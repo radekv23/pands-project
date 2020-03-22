ad753437d7d4538a5f5c5557ed307bc5b8845f80
R W <r.wojtczak23@gmail.com>
2020-03-22 12:56:52 +0000

    stats

--- analysis.py
+++ analysis.py
@@ -4,6 +4,24 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-#To read the CSV files, we use the read_csv method, as follows and pass the filename  to a variable (iris).
+#Reading the CSV files and passing the filename  to a variable named iris.
 iris = pd.read_csv("iris.csv")
 print(iris.head())
+
+
+#Printing information about dataset
+print(iris.info())
+
+#Checking number of columns and rows
+print(iris.shape)
+
+#Columns indexes
+print(iris.columns)
+
+#counting spiecies of iris
+print(iris["species"].value_counts())
+
+#Getting statistics about dataset
+print(iris.describe())
+
+ 

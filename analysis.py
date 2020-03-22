50b5488857b9430461e5e312f743863bc5515bc2
R W <r.wojtczak23@gmail.com>
2020-03-22 13:53:06 +0000

    2D Scatter Plots

--- analysis.py
+++ analysis.py
@@ -24,8 +24,16 @@ print(iris["species"].value_counts())
 #Getting statistics about dataset
 print(iris.describe())
 
+#1D Scatter Plots
+iris_setso = iris.loc[iris["species"] == "setosa"];
+iris_virginica = iris.loc[iris["species"] == "virginica"];
+iris_versicolor = iris.loc[iris["species"] == "versicolor"];
+plt.plot(iris_setso["petal_length"],np.zeros_like(iris_setso["petal_length"]), 'o')
+plt.plot(iris_versicolor["petal_length"],np.zeros_like(iris_versicolor["petal_length"]), 'o')
+plt.plot(iris_virginica["petal_length"],np.zeros_like(iris_virginica["petal_length"]), 'o')
+plt.grid()
+plt.show()  
 
-#Scatter Plots
-iris.plot(kind='scatter',x='sepal.length',y='sepal.width')
-plt.show()
-
+# 2D Scatter Plots
+iris.plot(kind="scatter",x="sepal_length",y="sepal_width")
+plt.show() 

5110f23cdbd90f1373c334bcfa8d3d8af86aaee5
R W <r.wojtczak23@gmail.com>
2020-03-29 20:44:42 +0100

    RandomForest

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
 
 #Reading the CSV files and passing the filename  to a variable named iris.
 iris = pd.read_csv("iris.csv")
@@ -17,6 +18,9 @@ print(iris.shape)
 
 #Columns indexes
 print(iris.columns)
+
+#Identifiy missing values
+pd.isnull(iris)
 
 #counting spiecies of iris
 print(iris["species"].value_counts())
@@ -49,4 +53,41 @@ plt.show()
 #Pair plots in seaborn
 sns.set_style("whitegrid")
 sns.pairplot(iris,hue="species",height=3)
+plt.show()
+
+
+#RandomForest algorithm
+# Isolate Data, class labels and column values
+X = iris.iloc[:,0:4]
+Y = iris.iloc[:,-1]
+names = iris.columns.values
+
+# Build the model
+model = RandomForestClassifier(n_estimators=100)
+
+# Fit the model
+model.fit(X, Y)
+
+# Print the results
+print("Features sorted by their score:")
+print(sorted(zip(map(lambda x: round(x, 4), model.feature_importances_), names), reverse=True))
+
+# Isolate feature importances 
+importance = model.feature_importances_
+
+# Sort the feature importances 
+sorted_importances = np.argsort(importance)
+
+# Insert padding
+padding = np.arange(len(names)-1) + 0.5
+
+# Plot the data
+plt.barh(padding, importance[sorted_importances], align='center')
+
+# Customize the plot
+plt.yticks(padding, names[sorted_importances])
+plt.xlabel("Relative Importance")
+plt.title("Variable Importance")
+
+# Show the plot
 plt.show()

53cee72a01c5a52ecf670e01e9eb63d8c462acac
R W <r.wojtczak23@gmail.com>
2020-04-23 19:50:05 +0100

    class

--- ml.py
+++ ml.py
@@ -103,9 +103,17 @@ print("Accuracy:",(accuracy_score(y_test, predictions)*100), "%")
 
 
 # Neural Network in PyTorch
+# Mapping
+mappings = {
+   'setosa': 0,
+   'versicolor': 1,
+   'virginica': 2
+}
+iris['species'] = iris['species'].apply(lambda x: mappings[x])
+
 # Train & Split
-X = iris.drop('Name', axis=1).values
-y = iris['Name'].values
+X = iris.drop('species', axis=1).values
+y = iris['species'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 X_train = torch.FloatTensor(X_train)
 X_test = torch.FloatTensor(X_test)
@@ -113,14 +121,17 @@ y_train = torch.LongTensor(y_train)
 y_test = torch.LongTensor(y_test)
 
 # Defining a NN Model
-class ANN(nn.Module):
+class INN(nn.Module):
    def __init__(self):
-       super().__init__()
-       self.fc1 = nn.Linear(in_features=4, out_features=16)
-       self.fc2 = nn.Linear(in_features=16, out_features=12)
-       self.output = nn.Linear(in_features=12, out_features=3)
+    super().__init__()
+    self.fc1 = nn.Linear(in_features=4, out_features=16)
+    self.fc2 = nn.Linear(in_features=16, out_features=12)
+    self.output = nn.Linear(in_features=12, out_features=3)
  
- def forward(self, x):
+   def forward(self, x):
      x = F.relu(self.fc1(x))
      x = F.relu(self.fc2(x))
-     x = self.out
+     x = self.output(x)
+     return x
+
+# Criterion & Optimizer
